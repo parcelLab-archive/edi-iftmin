e71afe30f3f33103179cd41a4109295f68a336f4
Julian Krenge <julian@parcellab.com>
2016-05-31 16:34:10 +0200

    Added segment TDT
    Segment TDT „transport information“ is also being parsed

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var edi = require('../index');
 var fs = require('fs');
 
-var file = fs.readFileSync('testfile/UPS-breaking.dat', 'utf8');
+var file = fs.readFileSync('testfile/UPS.dat', 'utf8');
 
 edi.inspectEdi(file);
